c1348a3dd9b7b1a7164415546e81fc3cf6f3abaf
MiguelAngelSaravia <miguel.saravia@wizeline.com>
2020-12-13 00:18:27 -0400

    add testing library and jest

--- src/components/Cards/Card.component.test.jsx
+++ src/components/Cards/Card.component.test.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import {CustomCard} from './Card.component';
 describe('CustomCard', () => {
-  const onClickSpy = jest.fn();
-  const props = Object.freeze({
-    children: <div onClick={onClickSpy}>Test Footer</div>,
-  });
+    it('sums 2 values', () => {
+        const num1 = 10
+        const num2 = 10
+
+        const result  = num1 + num2;
+        expect(result).toBe(20);
+    })
 });
